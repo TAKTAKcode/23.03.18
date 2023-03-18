5dfb12a332e8609d2dcd48e7a95bf3aaf5e4cab7
tak <wontaek22@gmail.com>
2023-03-18 21:57:47 +0900

    23.03.18
    백준 10813번 문제 (1차원 배열 : 공 바꾸기)

---
+++ src/com/tak/study/Main.java
@@ -0,0 +1,35 @@
+package com.tak.study;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] bag = new int[n+1];
+
+        for(int i = 1; i <= n; i++) {
+            bag[i]=i;
+        }
+
+        for(int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int temp=bag[a];
+
+            bag[a]=bag[b];
+            bag[b]=temp;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print(bag[i] + " ");
+        }
+
+    }
+
+}
